c7aa9749621e0113e8fc388622f9f439e30e3ccd
Hussain Rizvi <hussain2000.rizvi@gmail.com>
2022-11-18 17:42:04 +0500

    tet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useRef } from "react";
+import "./App.css";
 
 function App() {
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const iframe = useRef<any>();
+
+  const subscribe = (event: any) => {
+    const json = parse(event);
+    if (json?.source !== "readyplayerme") {
+      return;
+    }
+
+    // Susbribe to all events sent from Ready Player Me once frame is ready
+    if (json.eventName === "v1.frame.ready") {
+      iframe.current.contentWindow.postMessage(
+        JSON.stringify({
+          target: "readyplayerme",
+          type: "subscribe",
+          eventName: "v1.**",
+        }),
+        "*"
+      );
+    }
+
+    // Get avatar GLB URL
+    if (json.eventName === "v1.avatar.exported") {
+      console.log(`Avatar URL: ${json.data.url}`);
+      setAvatarUrl(json.data.url);
+    }
+
+    // Get user id
+    if (json.eventName === "v1.user.set") {
+      console.log(`User with id ${json.data.id} set: ${JSON.stringify(json)}`);
+    }
+  };
+
+  window.addEventListener("message", subscribe);
+  document.addEventListener("message", subscribe);
+
+  const parse = (event: any) => {
+    try {
+      return JSON.parse(event.data);
+    } catch (error) {
+      return null;
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <iframe
+        style={{
+          width: "100%",
+          height: "100vh",
+        }}
+        allow="camera *; microphone *"
+        src="https://demo.readyplayer.me/avatar?frameApi&clearCache"
+        title="Ready Player Me"
+        ref={iframe}
+      ></iframe>
     </div>
   );
 }
